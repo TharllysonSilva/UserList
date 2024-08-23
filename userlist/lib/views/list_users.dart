d14b0e75689b151357fa0c54fa6a82e80e41d7c3
TharllysonSilva <tharllysonsilva@gmail.com>
2024-08-22 23:07:24 -0300

    feat:lipstick: create views

---
+++ userlist/lib/views/list_users.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListUsers extends StatelessWidget {
+  const ListUsers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de usuarios'),
+      ),
+    );
+  }
+}
