cc3fcfbd418e65f259d2b8f837c0554229c7b696
TharllysonSilva <tharllysonsilva@gmail.com>
2024-08-22 22:56:20 -0300

    feat:sparkles: create data

---
+++ userlist/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:userlist/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(
+    name: 'Jessye',
+    email: 'jessye@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/18/20/15/woman-995164_1280.png',
+  ),
+  '2': User(
+    name: 'Nildelene',
+    email: 'branca@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/18/20/15/woman-995164_1280.png',
+  ),
+  '3': User(
+    name: 'Raimunda',
+    email: 'nonata@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2022/11/21/15/48/girl-7607431_1280.png',
+  ),
+  '4': User(
+    name: 'Tarllysson',
+    email: 'andré@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2024/05/09/11/37/abstract-8750691_1280.png',
+  ),
+};
