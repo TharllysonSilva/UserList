bf82492597d96e633b6fa8adde1bf6d9a1cfbe4a
TharllysonSilva <tharllysonsilva@gmail.com>
2024-09-04 18:15:32 -0300

    fix: Adding the ID Reference

--- userlist/lib/data/dummy_users.dart
+++ userlist/lib/data/dummy_users.dart
@@ -2,24 +2,28 @@ import 'package:userlist/models/user.dart';
 
 const dummyUsers = {
   '1': User(
+    id: '1',
     name: 'Jessye',
     email: 'jessye@gmail.com',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2015/10/18/20/15/woman-995164_1280.png',
   ),
   '2': User(
+    id: '2',
     name: 'Nildelene',
     email: 'branca@gmail.com',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2013/07/13/12/06/woman-159169_1280.png',
   ),
   '3': User(
+    id: '3',
     name: 'Raimunda',
     email: 'nonata@gmail.com',
     avatarUrl:
         'https://cdn.pixabay.com/photo/2022/11/21/15/48/girl-7607431_1280.png',
   ),
   '4': User(
+    id: '4',
     name: 'Tarllysson',
     email: 'andré@gmail.com',
     avatarUrl:
